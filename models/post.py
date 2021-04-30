37d31c14614acb1352f41f7c3beb096991992243
ErtanSarihan <iertansa@gmail.com>
2021-04-30 12:24:49 +0300

    initial commit

---
+++ models/post.py
@@ -0,0 +1,6 @@
+from mongoengine import Document, SequenceField, MultiLineStringField
+
+
+class Post(Document):
+    post_id = SequenceField(primary_key=True)
+    json_value = MultiLineStringField()
