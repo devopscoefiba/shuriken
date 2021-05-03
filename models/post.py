0640325b695e1d3cae86a658740f0184053de557
Başar Ballıöz <balliozbasar@gmail.com>
2021-05-03 14:52:17 +0300

    fix

--- models/post.py
+++ models/post.py
@@ -4,4 +4,4 @@ from mongoengine import Document, SequenceField, MultiLineStringField
 class Post(Document):
     post_id = SequenceField(primary_key=True)
     json_value = MultiLineStringField()
-    # meta = {'collection': 'posts'}
+    meta = {'collection': 'posts'}
