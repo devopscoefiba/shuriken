85adc563b0beb43e80fc4a554c1cf0dbd7faaaf3
Başar Ballıöz <balliozbasar@gmail.com>
2021-05-03 12:09:07 +0300

    branch bosaltildi

--- models/post.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import Document, SequenceField, MultiLineStringField
-
-
-class Post(Document):
-    post_id = SequenceField(primary_key=True)
-    json_value = MultiLineStringField()
