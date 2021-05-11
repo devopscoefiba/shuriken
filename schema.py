a06ab377ebd16f5c3a0d55c1a011f00ff7cbb079
Miracnur Yazkan <miracnuryazkan@users.noreply.github.com>
2021-05-11 12:14:03 +0300

    schema güncellendi

--- schema.py
+++ schema.py
@@ -5,74 +5,71 @@ from jsonschema import validate
 dataSchema = {
     #gerekirse her value icin description yazılabilinir
     "description": "DevOps yatayına girecek uygulamaların metadata bilgi seması",    
-    "type" : "object",    
-    "properties" : {
-        "$defs": {
-            "application" : {
-                "type" : "object",
-                "required" : [ "id" , "business" , "infrastructure" , "software" ],
-                "properties" : {
-                    "id" : {
-                        "type" : "integer"
-                    },
-                    "business" : {
-                        "type" : "object",
-                        "required" : [ "kurulum_saati", "is_birimi"],
-                        "properties" : {
-                            "kurulum_saati" : {
-                                "type" : "integer"  #simdilik integer dedim ama hata olabilir
-                            },
-                            "is_birimi" : {
-                                "type" : "string"
-                            }
+    "type" : ["object", "string", "integer"], 
+    "properties" : {        
+        "application" : {
+            "type" : "object",
+            "required" : [ "id" , "business" , "infrastructure" , "software" ],
+            "properties" : {
+                "id" : {
+                    "type" : "integer"
+                 },
+                "business" : {
+                    "type" : "object",
+                    "required" : [ "kurulum_saati", "is_birimi"],
+                    "properties" : {
+                        "kurulum_saati" : {
+                            "type" : "integer"  
+                        },
+                        "is_birimi" : {
+                            "type" : "string"
                         }
-                    },
-                    "infrastructure" : {
-                        "type" : "object",
-                        "required" : [ "sunucu_ismi" , "sunucu_teknoloji" , "middleware" , "direct_DBS" , "db_Tech_Client"],
-                        "properties" : {
-                            "sunucu_ismi" : {
-                                "type" : "string"
-                            },
-                            "sunucu_teknoloji" : {
-                                "type" : "string"
-                            },
-                            "middleware" : {
-                                "type" : "string"
-                            },
-                            "direct_DBS" : {
-                                "type" : "string"
-                            },
-                            "db_Tech_Client" : {
-                                "type" : "string"
-                            }
+                    }
+                },
+                "infrastructure" : {
+                    "type" : "object",
+                    "required" : [ "sunucu_ismi" , "sunucu_teknoloji" , "middleware" , "direct_DBS" , "db_Tech_Client"],
+                    "properties" : {
+                        "sunucu_ismi" : {
+                            "type" : "string"
+                        },
+                        "sunucu_teknoloji" : {
+                            "type" : "string"
+                        },
+                        "middleware" : {
+                            "type" : "string"
+                        },
+                        "direct_DBS" : {
+                            "type" : "string"
+                        },
+                        "db_Tech_Client" : {
+                            "type" : "string"
                         }
-                    },
-                    "software" : {
-                        "type" : "object",
-                        "required" : [ "gelistirici", "takim", "takim_direktoru" , "teknoloji", "repository"],
-                        "properties" : {
-                            "gelistirici" : {
-                                "type" : "string"
-                            },
-                            "takim" : {
-                                "type" : "string"
-                            },
-                            "takim_direktoru" : {
-                                "type" : "string"
-                            },
-                            "teknoloji" : {
-                                "type" : "string"
-                            },
-                            "repository" : {
-                                "type" : "string"
-                            }
-
+                    }
+                },
+                "software" : {
+                    "type" : "object",
+                    "required" : [ "gelistirici", "takim", "takim_direktoru" , "teknoloji", "repository"],
+                    "properties" : {
+                        "gelistirici" : {
+                            "type" : "string"
+                        },
+                        "takim" : {
+                            "type" : "string"
+                        },
+                        "takim_direktoru" : {
+                            "type" : "string"
+                        },
+                        "teknoloji" : {
+                            "type" : "string"
+                        },
+                        "repository" : {
+                            "type" : "string"
                         }
                     }
                 }
             }
-        }     
+        }             
     }
 }
 
@@ -86,12 +83,15 @@ def validateJson(jsonData):
         return False
     return True    
 
-#to use:
-#jsonData = json.loads('blablabla')
-#isValid = validateJson(jsonData)
-#if isValid:
-#   print(jsonData)
-#   print("verilen json gecerli")
-#else:
-#   print(jsonData)
-#   print("verilen json gecersiz")
+def test():
+    with open('json_template.txt', 'r') as j:
+        jsonData = json.dumps(j.read())
+        isValid = validateJson(jsonData)
+        if isValid:
+            print(jsonData)
+            response = "verilen json geçerli"
+        else:
+            print(jsonData)
+            response = "verilen json geçersiz"    
+    return response   
+
