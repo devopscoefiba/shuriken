abb87e516f654783b5afb76c271dd5fcb51b2b8e
Miracnur Yazkan <miracnuryazkan@users.noreply.github.com>
2021-05-11 17:22:35 +0300

    güncellendi

--- schema.py
+++ schema.py
@@ -81,6 +81,7 @@ def validateJson(jsonData):
     try :
         validate(instance=jsonData, schema=dataSchema)
     except jsonschema.exceptions.ValidationError as err:
+        print(err)
         return False
     return True    
 
